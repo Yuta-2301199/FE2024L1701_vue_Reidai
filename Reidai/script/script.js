b791e5f9cfe02f58576d0fbb00b677aa0e264c9f
Yuta-2301199 <2301199@s.asojuku.ac.jp>
2024-11-18 10:45:05 +0900

    課題の作成

--- Reidai/script/script.js
+++ Reidai/script/script.js
@@ -2,15 +2,27 @@
 new Vue({
     el: '#app',
     
-    data: {
+    data() {
+      return {
+        inputPassword: null,
+        password_confirmation: null 
+      };
     },
     computed: {
       // 入力されている、且つパスワードが一致しているかを確認する算出プロパティ
       // 入力されているかは、this.･･･ で判定可能
       // 例）一致TRUE（パスワード：aaaa、パスワード確認：aaaa）
       // 例）不一致FALSE（パスワード：aaaa、パスワード確認：aaab）
-
+      passwordCheck: function(){
+        if(this.inputPassword != null && this.password_confirmation != null && this.inputPassword == this.password_confirmation){
+          return true;
+        }
+      },
       // ボタンをTRUEなら登録を表示orFALSEなら使用不可を表示にする算出プロパティ
-
+      passwordCheckMissMatch: function(){
+        if(this.inputPassword != null && this.password_confirmation != null && this.inputPassword != this.password_confirmation){
+          return true;
+        }
+      }
     }
   });
